8fd904c2b7da0b5dc51a6b5ed74fb8a6f202bbb6
afif hendrawan <afif.hendrawan@gmail.com>
2017-07-13 11:38:24 +0700

    add welch

--- eegWelch.m
+++ eegWelch.m
@@ -1,3 +1,6 @@
 function output = eegWelch(x)
-  output = pwelch(x);
+  output = zeros(size(x,1), size(x,2));
+  output(1,:) = pwelch(x(1,:));
+  output(2,:) = pwlech(x(2,:));
+  output(3,:) = pwlech(x(3,:));
 end
